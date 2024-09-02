505b94d8c5e9eafce5a5ff3244173e7d55b48190
Goutam  Sharma <81623115+Goutam125262@users.noreply.github.com>
2024-09-02 17:23:44 +0000

    strings

---
+++ 01_basic/03_Converstion operation.js
@@ -0,0 +1,42 @@
+let score ="30abc";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// Number
+// "33"=>33
+// "33abc"=>NaN
+//  false =>0 //true =>1
+let isLogedIn ="sg";
+let booolValue=Boolean(isLogedIn)
+console.log(booolValue);
+console.log(typeof boolValue);
+
+
+
+//boolean
+//1 -> true
+//""->false
+//"abc"- >true
+
+
+let sameNumber=33
+let stringConvert=String(sameNumber)
+console.log(sameNumber);
+console.log(stringConvert);
+
+
+/*******************  opertions ********************/
+
+
+
+
+
+
+
+
---
+++ 01_basic/04_Strings.js
@@ -0,0 +1,17 @@
+// "Hello" + "World" = HelloWorld
+
+const name="hitesh";
+const count =50;
+
+// backticks => ``
+console.log(`Hello my name is ${name} and my repo cont is ${count}`)
+const string =new String("test");
+console.log(string.length);
+console.log(string.toLowerCase());
+console.log(string.charAt(2));
+console.log(string.indexOf('e'));
+
+
+
+
+
